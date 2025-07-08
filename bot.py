5f31ffc1e1699109dfd0e9f22d2fdd1aa624ee76
Selin Kıncal <selinkincal48@gmail.com>
2025-07-08 12:03:36 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+intents = discord.Intents.default()
+intents.message_content = True  # Mesaj içeriğini okuyabilmek için
+
+client =discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("Bot online ")
+    activity = discord.Game(name = "şu anda bunu yapıyor.")
+    await client.change_presence(activity=activity)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return 
+    
+    await message.channel.send(message.content)  
+
+
+client.run ("KENDİ TOKENİNİZİ BURAYA YAPIŞTIRIN")
+
